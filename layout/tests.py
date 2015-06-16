d5a3b6e1eb37883a16c7e98d2a1b7c98d8d67051
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-06-16 11:26:20 -0700

    Add test for home page html content

--- layout/tests.py
+++ layout/tests.py
@@ -1,4 +1,6 @@
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 
 from layout.views import home
@@ -9,3 +11,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home)
+
+    def test_home_page_returns_correct_html(self):
+        expected_html = render_to_string('home.html')
+        request = HttpRequest()
+        response = home(request)
+        actual_html = response.content.decode()
+        self.assertEqual(actual_html, expected_html)
