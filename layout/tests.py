0216bfd48fddb9bb7bda611ec5bdfe368bdee55f
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-06-16 11:22:14 -0700

    Add home page resolve test to layout

--- layout/tests.py
+++ layout/tests.py
@@ -1,3 +1,11 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
 
-# Create your tests here.
+from layout.views import home
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
