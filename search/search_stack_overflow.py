196c4506ba0ed2449741502e7ad27bf2a90e1740
Jan Van Bruggen <jancvanbruggen@gmail.com>
2015-06-18 11:41:45 -0700

    Attempt SO search with Py-StackExchange
    Use Py-StackExchange to search for relevant answers,
    but the API does not sort results by relevance

--- search/search_stack_overflow.py
+++ search/search_stack_overflow.py
@@ -1,4 +1,23 @@
-def get_relevant_stack_overflow_answers(query):
+import stackexchange
+
+
+stack_overflow = stackexchange.Site(stackexchange.StackOverflow,
+                                    impose_throttling=True)
+stack_overflow.be_inclusive()
+
+
+def get_relevant_stack_overflow_answers(query, num_answers=3):
     if not query:
         return tuple()
-    return 'answer 1', 'answer 2'
+    questions = stack_overflow.search(intitle=query)
+    questions = [stack_overflow.question(question.id)
+                 for question in questions]
+    questions.sort(key=lambda question: question.score, reverse=True)
+    questions = questions
+    answers = [answer
+               for question in questions
+               for answer in question.answers]
+    answers.sort(key=lambda answer: answer.score, reverse=True)
+    answer_bodies = [answer.body for answer in answers]
+    answer_bodies = answer_bodies[:num_answers]
+    return tuple(answer_bodies)
